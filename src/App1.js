045899041f1b8d093dfe499a448d7c1a0c868dd6
poornachandra1118 <poornachandrak2002@gmail.com>
2025-10-05 16:07:26 +0530

    App1.js

--- src/App1.js
+++ src/App1.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Reg from './chat_pages/Reg'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import Index from './chat_pages/Index'
 import Scrollbarchat from './chat_pages/Scrollbarchat'
 import Samplepage from './pages/Samplepage'
