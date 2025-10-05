08faa89d1aac3024bf418a22c0368d5907159836
poornachandra1118 <poornachandrak2002@gmail.com>
2025-10-05 16:17:45 +0530

    Navbar.jsx

--- src/chat_pages/Navbar.jsx
+++ src/chat_pages/Navbar.jsx
@@ -7,13 +7,11 @@ const Navbar = () => {
     <div class="container-fluid">
         <a class="navbar-brand" href="#">Secret Box</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+        <span class="navbar-toggler-icon"></span
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            {/* <li class="nav-item">
-            <Link class="nav-link active" aria-current="page" to='/Dashboard'>Dashboard</Link>
-            </li> */}
+           
            
 
         </ul>
