2c3eb89965e4e92e7276c5127a724247472d497e
poornachandra1118 <poornachandrak2002@gmail.com>
2025-10-05 15:04:34 +0530

    Dashboard.jsx

--- src/chat_pages/Dashboard.jsx
+++ src/chat_pages/Dashboard.jsx
@@ -194,9 +194,9 @@ const Dashboard = () => {
 
                 (String(data[x].from_mobile[0]) === String(chat_info.from_mobile)) ?
                messages.push(
-                { type: 'from', text:data[x].text[0]  },
+                { type: 'from', text:data[x].text[0] , time: '' },
                ): messages.push(
-                { type: 'to', text:data[x].text[0]},
+                { type: 'to', text:data[x].text[0] , time: ''},
                )
             }
              }
